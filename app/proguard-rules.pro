8d87af1fc8ed073d05db510e02e086fa313f0d3e
Utsav <mrutsav2024@gmail.com>
2026-09-25 19:43:09 +0530

    Migrate to A code

--- app/proguard-rules.pro
+++
@@ -1,2 +0,0 @@
-# Project-specific R8 rules. Add keep rules here as libraries that need them
-# (JGit, Sora Editor, ...) are introduced in later milestones.
--- app/src/main/java/com/invictus/xcode/ui/navigation/Routes.kt
+++
@@ -1,8 +0,0 @@
-package com.invictus.xcode.ui.navigation
-
-object Routes {
-    const val PERMISSION = "permission"
-    const val WORKSPACE = "workspace"
-    const val EDITOR = "editor"
-    const val SETTINGS = "settings"
-}
